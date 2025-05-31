8ca7a275b735f6c9e2a66ed2466a18bcd4144fc3
Cristian <Arcezx>
2025-05-31 13:53:19 +0200

    Actualizado incidenteController y CORS

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../enviroments/enviroment.prod' 
+import { HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:8080/api'; // URL del backend
+  private baseUrl = `${environment.apiBaseUrl}/api`; // Usa la URL de environment
 
   constructor(private http: HttpClient) {}
 
- enviarIncidente(data: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/incidentes`, data);
-  }
+enviarIncidente(data: any): Observable<any> {
+  return this.http.post(`${this.baseUrl}/incidentes`, data, {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  });
+}
 }
--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,  // ¡Debe ser true en producción!
-  apiBaseUrl: 'https://tu-api-real.com'  // Cambia esto por tu URL real de producción
+  apiBaseUrl: 'https://arcefly-backend.onrender.com'  // Cambia esto por tu URL real de producción
 };
